66b5cf323468cc56be395e44fff6793c3de89001
pedrovinhas <pstvideos@gmail.com>
2022-10-30 23:09:58 -0300

    add: component and story: Follow

---
+++ src/components/Follow.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Follow, FollowProps } from  './Follow';
+
+export default {
+    title: 'Components/Follow',
+    component: Follow,
+    args: {
+    },
+    argTypes: {}
+} as Meta<FollowProps>
+
+export const Default: StoryObj<FollowProps> = {}
+

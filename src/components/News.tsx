fab7fa84c0d20d2f92c92a99fa042f65c40f0c13
pedrovinhas <pstvideos@gmail.com>
2022-11-01 11:41:49 -0300

    add: component and stories: News

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Heading } from "./Heading";
 import { Text } from './Text'
 
@@ -7,13 +8,13 @@ export interface NewsProps {
 
 export function News(props: NewsProps) {
     return (
-        <div className='bg-dark-8 flex flex-col w-[350px] rounded-2xl dark:bg-dark-2'>
+        <div className='bg-dark-8 flex flex-col w-[350px] rounded-2xl dark:bg-dark-2 '>
             <div className='pt-[11px] pb-[11px] border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5'>
                 <Heading size="xl" className='ml-4 dark:text-white' >
                     What's Happening
                 </Heading>
             </div>
-            <a className="flex justify-center gap-4 pt-2 pb-2 cursor-pointer border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5 hover:bg-dark-4  transition-all duration-150"> 
+            <a className="flex justify-center gap-4 pt-2 pb-2 cursor-pointer border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5 hover:bg-dark-7 dark:hover:bg-dark-4  transition-all duration-150"> 
                 <div className="ml-4 w-[235px]">
                     <div className="flex gap-1">
                         <span className="text-sm font-medium text-dark-5"  >
@@ -42,7 +43,7 @@ export function News(props: NewsProps) {
                     {/* Thumbnail Component? */}
                 </div>
             </a>
-            <a className="flex justify-center gap-4 pt-2 pb-2 cursor-pointer border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5 hover:bg-dark-4  transition-all duration-150"> 
+            <a className="flex justify-center gap-4 pt-2 pb-2 cursor-pointer border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5 hover:bg-dark-7 dark:hover:bg-dark-4  transition-all duration-150"> 
                 <div className="ml-4 w-[235px]">
                     <div className="flex gap-1">
                         <span className="text-sm font-medium text-dark-5"  >
@@ -67,11 +68,11 @@ export function News(props: NewsProps) {
                     </div>
                 </div>
                 <div className="w-16 h-16">
-                    <img src="../../public/thumbnail-1.png" alt="" className="w-16 h-16 rounded-xl" /> 
+                    <img src="../../public/thumbnail-2.png" alt="" className="w-16 h-16 rounded-xl" /> 
                     {/* Thumbnail Component? */}
                 </div>
             </a>
-            <a className="flex justify-center gap-4 pt-2 pb-2 cursor-pointer border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5 hover:bg-dark-4  transition-all duration-150">
+            <a className="flex justify-center gap-4 pt-2 pb-2 cursor-pointer border-b-dark-7 border-[1px] border-t-0 border-l-0 border-r-0 border-opacity-5 hover:bg-dark-7 dark:hover:bg-dark-4  transition-all duration-150">
                 <div className="ml-4 w-[235px]">
                     <div className="flex gap-1">
                         <span className="text-sm font-medium text-dark-5"  >
@@ -96,14 +97,14 @@ export function News(props: NewsProps) {
                     </div>
                 </div>
                 <div className="w-16 h-16">
-                    <img src="../../public/thumbnail-1.png" alt="" className="w-16 h-16 rounded-xl" /> 
+                    <img src="../../public/thumbnail-3.png" alt="" className="w-16 h-16 rounded-xl" /> 
                     {/* Thumbnail Component? */}
                 </div>
             </a>
-            <a href="" className='pt-3 pb-3 rounded-b-2xl hover:bg-dark-4  duration-300'>
+            <Link to='*' className='pt-3 pb-3 rounded-b-2xl hover:bg-dark-7 dark:hover:bg-dark-4  duration-300'>
                 <span className='text-primary-blue ml-6 font-medium text-md'> Show more </span>
                 {/* Link Component? */}
-            </a>
+            </Link>
         </div>
     )
 }

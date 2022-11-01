0903d795071dccfbaa83846168be7477b30761b7
pedrovinhas <pstvideos@gmail.com>
2022-11-01 11:44:44 -0300

    adjust: app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Aside } from './components/Aside'
+import { Footer } from './components/Footer'
+import { Main } from './components/Main'
+import { SideMenu } from './components/SideMenu'
+import PathRoutes from './routes'
 import './styles/global.css'
 
 export function App() {
   return (
-   <h1> Hello World </h1>
+    <PathRoutes></PathRoutes>
+  //   <div className='flex items-center flex-col dark:bg-dark-1 '>
+  //   <div className='flex '>
+  //     <SideMenu/>
+  //     <Main />
+  //     <Aside/>
+  //  </div>
+  //    <Footer/>
+  //  </div>
   )
 }
